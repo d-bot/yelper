3e147bb455e0a9320cbff7a3faca6d878a3aef6e
dch <dch@dch.io>
2014-07-13 02:07:27 +0000

    update

--- lib/yelper/crawler.rb
+++ lib/yelper/crawler.rb
@@ -3,24 +3,67 @@ require 'open-uri'
 
 module Yelper
 
-keyword = ARGV[0].gsub(' ', '+')
-location = ARGV[1].gsub(' ', '+')
-
 	class Crawler
 
-		attr_reader :doc
-		UA = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/537.75.14"
+		attr_reader :doc, :keyword, :location, :result
 
-		def initialize(keyworld, location)
-			url = "http://www.yelp.com/search?find_desc=#{keyword}&find_loc=#{location}%2C+CA&ns=1"
-			@doc = Nokogiri::HTML(open("#{url}", "User-Agent" => "#{UA}"))
+		UA = "mozilla/5.0 (macintosh; intel mac os x 10_9_2) applewebkit/537.75.14 (khtml, like gecko) version/7.0.3 safari/537.75.14"
+
+		def initialize(keyword, location)
+			@keyword = keyword
+			@location = location
+			@result = []
+
+			url = "http://www.yelp.com/search?find_desc=#{@keyword}&find_loc=#{@location}%2c+ca&ns=1"
+			@doc = Nokogiri::HTML(open("#{url}", "user-agent" => "#{UA}"))
 		end
 
-		def build_data
+
+		def start_crawl
+
 			total = @doc.xpath("//span[@class = 'pagination-results-window']").collect do |node|
 				node.text.strip.split('of')[1].strip
 			end
-			total = total[0].to_str if total.length == 1
+
+			tot = total[0].to_i if total.length == 1
+
+			(tot / 10 + 1).times do |i|
+				i *= 10
+				url = "http://www.yelp.com/search?find_desc=#{@keyword}&find_loc=#{@location}%2c+ca&ns=1&start=#{i}"
+				@doc = Nokogiri::HTML(open("#{url}", "user-agent" => "#{UA}"))
+
+				name = @doc.xpath("//div[@class = 'media-story']/h3[@class = 'search-result-title']/span[@class = 'indexed-biz-name']/a").collect do |node|
+					node.text.strip
+				end
+
+				review = @doc.xpath("//span[@class = 'review-count rating-qualifier']").collect do |node|
+					node.text.strip
+				end
+
+				star = @doc.xpath("//div[@class = 'rating-large']/i[@class]").collect do |node|
+					node.attr("class")
+				end
+
+				star = star.map { |attr| attr.split[1] }
+
+				price = @doc.xpath("//span[@class = 'business-attribute price-range']").collect do |node|
+					node.text.strip
+				end
+
+				address = @doc.xpath("//address").collect do |node|
+					node.text.strip
+				end
+
+				ret = name.zip review, star, price, address
+				@result << ret
+
+			end
+			p @result
+
+		end
+
+=begin
+		def Crawler.parse
 
 			name = @doc.xpath("//div[@class = 'media-story']/h3[@class = 'search-result-title']/span[@class = 'indexed-biz-name']/a").collect do |node|
 				node.text.strip
@@ -44,10 +87,10 @@ location = ARGV[1].gsub(' ', '+')
 				node.text.strip
 			end
 
-			ret = name.zip review, star, price, address
+			@ret = name.zip review, star, price, address
 
-			return ret
 		end
+=end
 
 	end
 
---
+++ lib/yelper/filter.rb
@@ -0,0 +1,18 @@
+module Yelper
+
+	class Filter
+		def initialize(huge_array)
+			#do somethng
+		end
+
+		def more_than_500_review
+
+		end
+
+		def more_than_4_stars
+
+		end
+
+	end
+
+end
